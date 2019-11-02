cc42041133702bb771d9a56a7aa0f463a16410d3
vutaka <vutaka0120@gmail.com>
2019-11-02 22:57:44 +0900

    ボタンを配置

--- src/container/pages/GameCreate.js
+++ src/container/pages/GameCreate.js
@@ -3,9 +3,12 @@ import { PreparationTemplate } from "../templates/PreparationTemplate";
 import { Field } from "../../components/atoms/Field";
 import { Input } from "../../components/atoms/Input";
 import { SelectForList } from "../../components/atoms/SelectForList";
+import { ButtonField } from "../../components/atoms/ButtonField";
+import { Button } from "../../components/atoms/Button";
 
-export function GameCreate(props) {
+export function GameCreate() {
   const list = Array.from(Array(5), (v, k) => {return {value: k + 2, text: (k + 2) + "人"}});
+  const createGame = () => {window.alert("呼び出し")};
   return(
     <PreparationTemplate title="ゲームを主催する">
       <Field label="あなたのあだ名">
@@ -15,8 +18,9 @@ export function GameCreate(props) {
         <SelectForList
           list={list}/>
       </Field>
+      <ButtonField>
+        <Button onClick={createGame}>決定</Button>
+      </ButtonField>
     </PreparationTemplate>
-
-
   )
 }
