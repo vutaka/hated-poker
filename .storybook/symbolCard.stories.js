defbba4fe24d9646454450d19a2ebc3da482cf7c
vutaka <vutaka0120@gmail.com>
2019-11-07 00:50:34 +0900

    カードを追加

---
+++ .storybook/symbolCard.stories.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { SymbolCard } from "../src/presenter/atoms/SymbolCard";
+
+export default { title: "SymbolCard" };
+
+export const normal = () => <>
+  <div style={{ display: "grid", "grid-template-columns": "repeat(8, 1fr)" }}>
+    <SymbolCard isOpen={true} symbolName={"rat"} />
+    <SymbolCard isOpen={true} symbolName={"dog"} />
+    <SymbolCard isOpen={true} symbolName={"snake"} />
+    <SymbolCard isOpen={true} symbolName={"jellyfish"} />
+    <SymbolCard isOpen={true} symbolName={"bat"} />
+    <SymbolCard isOpen={true} symbolName={"deer"} />
+    <SymbolCard isOpen={true} symbolName={"shark"} />
+    <SymbolCard isOpen={true} symbolName={"crow"} />
+  </div>
+  <div style={{ display: "grid", "grid-template-columns": "repeat(8, 1fr)" }}>
+    <SymbolCard symbolName={"rat"} />
+    <SymbolCard symbolName={"dog"} />
+    <SymbolCard symbolName={"snake"} />
+    <SymbolCard symbolName={"jellyfish"} />
+    <SymbolCard symbolName={"bat"} />
+    <SymbolCard symbolName={"deer"} />
+    <SymbolCard symbolName={"shark"} />
+    <SymbolCard symbolName={"crow"} />
+  </div>
+</>;
+
+export const small = () => <>
+  <div style={{ display: "grid", "grid-template-columns": "repeat(8, 1fr)" }}>
+    <SymbolCard isSmall={true} isOpen={true} symbolName={"rat"} />
+    <SymbolCard isSmall={true} isOpen={true} symbolName={"dog"} />
+    <SymbolCard isSmall={true} isOpen={true} symbolName={"snake"} />
+    <SymbolCard isSmall={true} isOpen={true} symbolName={"jellyfish"} />
+    <SymbolCard isSmall={true} isOpen={true} symbolName={"bat"} />
+    <SymbolCard isSmall={true} isOpen={true} symbolName={"deer"} />
+    <SymbolCard isSmall={true} isOpen={true} symbolName={"shark"} />
+    <SymbolCard isSmall={true} isOpen={true} symbolName={"crow"} />
+  </div>
+  <div style={{ display: "grid", "grid-template-columns": "repeat(8, 1fr)" }}>
+    <SymbolCard isSmall={true} symbolName={"rat"} />
+    <SymbolCard isSmall={true} symbolName={"dog"} />
+    <SymbolCard isSmall={true} symbolName={"snake"} />
+    <SymbolCard isSmall={true} symbolName={"jellyfish"} />
+    <SymbolCard isSmall={true} symbolName={"bat"} />
+    <SymbolCard isSmall={true} symbolName={"deer"} />
+    <SymbolCard isSmall={true} symbolName={"shark"} />
+    <SymbolCard isSmall={true} symbolName={"crow"} />
+  </div>
+</>;
---
+++ src/presenter/atoms/SymbolCard.js
@@ -0,0 +1,41 @@
+import React from "react"
+import styles from "./SymbolCard.module.scss";
+import classnames from "classnames"
+
+const getSymbolName = (name) => {
+  switch (name) {
+    case "rat":
+      return styles.rat;
+    case "dog":
+      return styles.dog;
+    case "snake":
+      return styles.snake;
+    case "jellyfish":
+      return styles.jellyfish;
+    case "bat":
+      return styles.bat;
+    case "deer":
+      return styles.deer;
+    case "shark":
+      return styles.shark;
+    case "crow":
+      return styles.crow;
+    default:
+      break;
+  }
+}
+
+export function SymbolCard(props) {
+
+  return (
+    <div
+      className={classnames(styles.card,
+        { [styles.small]: props.isSmall },
+        { [styles.open]: props.isOpen },
+        props.additionalClassName
+      )}>
+      <div className={classnames(styles.front, getSymbolName(props.symbolName))} />
+      <div className={styles.back}></div>
+    </div >
+  )
+}
