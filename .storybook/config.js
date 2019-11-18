f8d326097d38eb6f49e6211b13544ea6b036ba86
vutaka <vutaka0120@gmail.com>
2019-11-18 23:35:50 +0900

    storybookのaddon追加

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,12 @@
-import { configure } from "@storybook/react";
+import { configure, addParameters } from "@storybook/react";
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 import "modern-css-reset";
 import "../src/style/global.scss"
 
+
+addParameters({
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+  },
+});
 configure(require.context("./", true, /\.stories\.js$/), module);
